df3872899f0c28de97acce9c08faebde8f53bc48
Jasem Valencia <jasemvalencia@gmail.com>
2021-11-04 14:42:59 -0500

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,102 +1,93 @@
-import { orderBy } from 'lodash';
 import './style.css';
 
-const arrObjTask=[
-    {
-      description: "wash the dishes",
-      completed: true,
-      index: 3,
-    },
-    {
-      description: "complete to do list PR",
-      completed: false,
-      index: 1,
-    },
-    {
-      description: "wash my clothes",
-      completed: false,
-      index: 2,
-    }
+const arrObjTask = [
+  {
+    description: 'wash the dishes',
+    completed: true,
+    index: 3,
+  },
+  {
+    description: 'complete to do list PR',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'wash my clothes',
+    completed: false,
+    index: 2,
+  },
 ];
 
+function loadTask(arr) {
+  const ulTask = document.getElementById('ulTask');
 
-function loadTask(arr){
-    const ulTask=document.getElementById('ulTask');
-    
-    const lsTaskInsert= document.createElement('li');
-    lsTaskInsert.classList="list-group-item";    
-    ulTask.appendChild(lsTaskInsert);
+  const lsTaskInsert = document.createElement('li');
+  lsTaskInsert.classList = 'list-group-item';
+  ulTask.appendChild(lsTaskInsert);
 
-    const itemGroupInsert=document.createElement('div');
-    itemGroupInsert.classList="input-group";
-    lsTaskInsert.appendChild(itemGroupInsert);
+  const itemGroupInsert = document.createElement('div');
+  itemGroupInsert.classList = 'input-group';
+  lsTaskInsert.appendChild(itemGroupInsert);
 
-    const txtTaskName=document.createElement('input');
-    txtTaskName.classList="form-control fw-lighter";
-    txtTaskName.placeholder="Add to your list ...";
-    itemGroupInsert.appendChild(txtTaskName);
+  const txtTaskName = document.createElement('input');
+  txtTaskName.classList = 'form-control fw-lighter';
+  txtTaskName.placeholder = 'Add to your list ...';
+  itemGroupInsert.appendChild(txtTaskName);
 
-    const btnInsertTask=document.createElement('button');
-    btnInsertTask.classList="btn btn-outline-secondary";    
-    itemGroupInsert.appendChild(btnInsertTask);
+  const btnInsertTask = document.createElement('button');
+  btnInsertTask.classList = 'btn btn-outline-secondary';
+  itemGroupInsert.appendChild(btnInsertTask);
 
-    const iconInsert=document.createElement('i');
-    iconInsert.classList="bi bi-arrow-down";
-    btnInsertTask.appendChild(iconInsert);
+  const iconInsert = document.createElement('i');
+  iconInsert.classList = 'bi bi-arrow-down';
+  btnInsertTask.appendChild(iconInsert);
 
-    
-    
-    for(let i=0; i < arr.length; i++){
-      const lsTask= document.createElement('li');
-      lsTask.classList="list-group-item";
-      
-      const itemGroup=document.createElement('div');
-      itemGroup.classList="input-group";
+  for (let i = 0; i < arr.length; i += 1) {
+    const lsTask = document.createElement('li');
+    lsTask.classList = 'list-group-item';
 
-      const itemGroupText=document.createElement('div');
-      itemGroupText.classList="input-group-text";
-      
-      const chkCompleted=document.createElement('input');
-      chkCompleted.type="checkbox";      
-      chkCompleted.checked=arr[i].completed;
-      chkCompleted.classList="form-check-input mt-0";
-      
+    const itemGroup = document.createElement('div');
+    itemGroup.classList = 'input-group';
 
-      const txtDescription=document.createElement('input');
-      txtDescription.value=arr[i].description;
-      txtDescription.classList="form-control"
+    const itemGroupText = document.createElement('div');
+    itemGroupText.classList = 'input-group-text';
 
-      const btnDeleteItem=document.createElement('button');      
-      btnDeleteItem.classList="btn btn-outline-secondary";
+    const chkCompleted = document.createElement('input');
+    chkCompleted.type = 'checkbox';
+    chkCompleted.checked = arr[i].completed;
+    chkCompleted.classList = 'form-check-input mt-0';
 
-      const iconTrash=document.createElement('i');
-      iconTrash.classList="bi bi-trash"
-  
-      lsTask.appendChild(itemGroup);
-      itemGroup.appendChild(itemGroupText);
-      itemGroupText.appendChild(chkCompleted);
-      itemGroup.appendChild(txtDescription);
-      itemGroup.appendChild(btnDeleteItem);
-      btnDeleteItem.appendChild(iconTrash);
-      
-      document.getElementById('ulTask').appendChild(lsTask);
-      
-    };
+    const txtDescription = document.createElement('input');
+    txtDescription.value = arr[i].description;
+    txtDescription.classList = 'form-control';
 
-    const btnDelete=document.createElement('button');
-    btnDelete.classList="btn btn-outline-secondary";
-    btnDelete.innerHTML="Clear all completed ";
-    
-    
-    ulTask.appendChild(btnDelete);
-};
+    const btnDeleteItem = document.createElement('button');
+    btnDeleteItem.classList = 'btn btn-outline-secondary';
 
+    const iconTrash = document.createElement('i');
+    iconTrash.classList = 'bi bi-trash';
 
-function ReorderTask(arrObjTask ){
-  return _.sortBy(arrObjTask, 'index');
-};
+    lsTask.appendChild(itemGroup);
+    itemGroup.appendChild(itemGroupText);
+    itemGroupText.appendChild(chkCompleted);
+    itemGroup.appendChild(txtDescription);
+    itemGroup.appendChild(btnDeleteItem);
+    btnDeleteItem.appendChild(iconTrash);
 
-const arrObjTaskReorder=ReorderTask(arrObjTask);
+    document.getElementById('ulTask').appendChild(lsTask);
+  }
 
+  const btnDelete = document.createElement('button');
+  btnDelete.classList = 'btn btn-outline-secondary';
+  btnDelete.innerHTML = 'Clear all completed ';
+
+  ulTask.appendChild(btnDelete);
+}
+
+function ReorderTask(arrObjTask) {
+  return arrObjTask.sort((a, b) => ((a.index > b.index) ? 1 : -1));
+}
+
+const arrObjTaskReorder = ReorderTask(arrObjTask);
 
 loadTask(arrObjTaskReorder);
