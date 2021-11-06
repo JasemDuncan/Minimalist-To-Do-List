a5f3e624d0509d5b57429191f8234d2c77a6b800
Jasem Valencia <jasemvalencia@gmail.com>
2021-11-06 11:57:23 -0500

    Implement CRUD functions, all new taks have completed false when are addings, when a item was deleted all

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import './style.css';
 import * as aux from './statusUpdate';
+import {
+  AddNewTask, ClearAllCompleted, DeleteTask, UpdateTaskDescription,
+} from './crud';
 
-const arrObjTask = [
-  {
-    description: 'wash the dishes',
-    completed: true,
-    index: 3,
-  },
-  {
-    description: 'complete to do list PR',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'wash my clothes',
-    completed: false,
-    index: 2,
-  },
-];
+const arrObjTask = [];
 
 function ReorderTask(arrObjTask) {
   return arrObjTask.sort((a, b) => ((a.index > b.index) ? 1 : -1));
@@ -29,11 +16,16 @@ function saveLocalStorage(arrToSave) {
   window.localStorage.setItem('MinimalistToDoList', JSON.stringify(arrToSave));
 }
 
+function loadLocalStorage() {
+  return JSON.parse(window.localStorage.getItem('MinimalistToDoList'));
+}
+
 function loadTask(arr) {
   const ulTask = document.getElementById('ulTask');
 
   const lsTaskInsert = document.createElement('li');
   lsTaskInsert.classList = 'list-group-item';
+  lsTaskInsert.id = 'idlsTaskInsert';
   ulTask.appendChild(lsTaskInsert);
 
   const itemGroupInsert = document.createElement('div');
@@ -42,11 +34,41 @@ function loadTask(arr) {
 
   const txtTaskName = document.createElement('input');
   txtTaskName.classList = 'form-control fw-lighter';
+  txtTaskName.id = 'txtTaskName';
   txtTaskName.placeholder = 'Add to your list ...';
+
+  txtTaskName.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      const newRowTask = AddNewTask(txtTaskName.value, loadLocalStorage());
+      saveLocalStorage(newRowTask);
+      const myChilds = window.document.getElementById('ulTask');
+      myChilds.innerHTML = `
+      <div class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Today's To Do</h5>                          
+        </div>                       
+      </div>`;
+      loadTask(loadLocalStorage(newRowTask));
+    }
+  });
   itemGroupInsert.appendChild(txtTaskName);
 
   const btnInsertTask = document.createElement('button');
   btnInsertTask.classList = 'btn btn-outline-secondary';
+  btnInsertTask.id = 'btnInsertTask';
+  btnInsertTask.addEventListener('click', () => {
+    const newRowTask = AddNewTask(txtTaskName.value, loadLocalStorage());
+    saveLocalStorage(newRowTask);
+    const myChilds = window.document.getElementById('ulTask');
+    myChilds.innerHTML = `
+    <div class="list-group-item list-group-item-action">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">Today's To Do</h5>                          
+      </div>                       
+    </div>`;
+    loadTask(loadLocalStorage(newRowTask));
+  });
+
   itemGroupInsert.appendChild(btnInsertTask);
 
   const iconInsert = document.createElement('i');
@@ -69,20 +91,50 @@ function loadTask(arr) {
     chkCompleted.checked = arr[i].completed;
     chkCompleted.classList = 'form-check-input mt-0';
 
-    chkCompleted.addEventListener('change', () => {
-      const arrAux = aux.statusUpdate(arr, arr[i].index);
-      saveLocalStorage(arrAux);
-    });
-
     const txtDescription = document.createElement('input');
     txtDescription.value = arr[i].description;
     txtDescription.classList = 'form-control';
+
+    chkCompleted.addEventListener('change', () => {
+      const arrAux = aux.statusUpdate(arr, arr[i].index);
+      saveLocalStorage(arrAux);
+
+      if (arr[i].completed) {
+        txtDescription.classList = 'line-through form-control';
+      } else {
+        txtDescription.classList = 'form-control';
+      }
+    });
+
+    txtDescription.addEventListener('input', () => {
+      const arrAux2 = UpdateTaskDescription(txtDescription.value, arr, arr[i].index);
+      saveLocalStorage(arrAux2);
+    });
+
+    if (arr[i].completed) {
+      txtDescription.classList = 'line-through form-control';
+    } else {
+      txtDescription.classList = 'form-control';
+    }
 
     const btnDeleteItem = document.createElement('button');
     btnDeleteItem.classList = 'btn btn-outline-secondary';
 
     const iconTrash = document.createElement('i');
     iconTrash.classList = 'bi bi-trash';
+
+    btnDeleteItem.addEventListener('click', () => {
+      const arrAux3 = DeleteTask(arr[i].index, arr);
+      saveLocalStorage(arrAux3);
+      const myChilds = window.document.getElementById('ulTask');
+      myChilds.innerHTML = `
+      <div class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Today's To Do</h5>                          
+        </div>                       
+      </div>`;
+      loadTask(loadLocalStorage(arrAux3));
+    });
 
     lsTask.appendChild(itemGroup);
     itemGroup.appendChild(itemGroupText);
@@ -92,17 +144,28 @@ function loadTask(arr) {
     btnDeleteItem.appendChild(iconTrash);
 
     document.getElementById('ulTask').appendChild(lsTask);
+    txtTaskName.focus();
   }
 
   const btnDelete = document.createElement('button');
   btnDelete.classList = 'btn btn-outline-secondary';
   btnDelete.innerHTML = 'Clear all completed ';
 
-  ulTask.appendChild(btnDelete);
-}
+  btnDelete.addEventListener('click', () => {
+    const arrAux4 = ClearAllCompleted(arr);
+    saveLocalStorage(arrAux4);
+    const myChilds = window.document.getElementById('ulTask');
+    myChilds.innerHTML = `
+    <div class="list-group-item list-group-item-action">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">Today's To Do</h5>                          
+      </div>                       
+    </div>`;
 
-function loadLocalStorage() {
-  return JSON.parse(window.localStorage.getItem('MinimalistToDoList'));
+    loadTask(loadLocalStorage(arrAux4));
+  });
+
+  ulTask.appendChild(btnDelete);
 }
 
 function checkLocalStorage() {
@@ -112,7 +175,8 @@ function checkLocalStorage() {
 window.onload = () => {
   if (checkLocalStorage()) {
     const arrOTRLS = loadLocalStorage();
-    loadTask(arrOTRLS);
+    const arrOTRLS2 = ReorderTask(arrOTRLS);
+    loadTask(arrOTRLS2);
   } else {
     saveLocalStorage(arrObjTaskReorder);
     loadTask(arrObjTaskReorder);
